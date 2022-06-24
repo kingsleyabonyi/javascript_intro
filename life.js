2af05c9e6881aa7d5fa35769713f31991fece504
Kingsley Abonyi <52834209+kingsleyabonyi@users.noreply.github.com>
2022-06-24 08:23:00 +0100

    Add files via upload

---
+++ life.js
@@ -0,0 +1,8 @@
+//function that prints my name, height and country
+
+let name = "Nnamdi Abonyi";
+let height = "75cm";
+var country = "Nigeria";
+
+alert( 'Name: ' + name + '\n' + 'Height: ' + height + '\n' + 'Country: ' + country);
+
